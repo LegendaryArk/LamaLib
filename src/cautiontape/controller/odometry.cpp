ccb55c817516ad3d96ef624ee596dc201f423f64
Noah <noahsun0418@gmail.com>
2022-03-12 14:08:08 -0500

    finished the math; still need to do the tuning

---
+++ src/cautiontape/controller/odometry.cpp
@@ -0,0 +1,94 @@
+#include "odometry.hpp"
+#include <math.h>
+
+using namespace lamaLib;
+
+Odometry::Odometry(ADIEncoder leftEncoder, ADIEncoder rightEncoder, ADIEncoder rearEncoder, OdomScales scales, int tpr):
+                    leftEncoder(leftEncoder), rightEncoder(rightEncoder), rearEncoder(rearEncoder), scales(scales), tpr(tpr) {
+    // Default coordinate position
+    pose = {0, 0, 0, pros::millis()};
+
+    // Start odometry task
+    startOdom();
+}
+
+EncoderValues Odometry::getEncoders() {
+    return {(double) leftEncoder.get_value(), (double) rightEncoder.get_value(), (double) rearEncoder.get_value(), 0};
+}
+
+Pose Odometry::getPose() {
+    return pose;
+}
+void Odometry::setPose(Pose ipose) {
+    pose = ipose;
+}
+
+OdomScales Odometry::calibrate() {
+    
+}
+
+OdomScales Odometry::getScales() {
+    return scales;
+}
+void Odometry::setScales(OdomScales iscales) {
+    scales = iscales;
+}
+
+void Odometry::startOdom() {
+    odom = c::task_create(odometry, this, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "odometry");
+}
+void Odometry::endOdom() {
+    c::task_delete(odom);
+}
+
+void odometry(void* param) {
+    Odometry* odom = (Odometry*) param;
+
+    OdomScales scales = odom->getScales();
+
+    double chassisDiameter = scales.leftRadius + scales.rightRadius;
+
+    EncoderValues readings = odom->getEncoders();
+    EncoderValues readingsPrev = odom->prev;
+    EncoderValues readingsDiff;
+
+    readingsDiff.left = readings.left - readingsPrev.left;
+    readingsDiff.right = readings.right - readingsPrev.right;
+    readingsDiff.rear = readings.rear - readingsPrev.rear;
+    
+    // Delta distance
+    EncoderValues delta;
+    delta.left = (readingsDiff.left / odom->tpr) * (chassisDiameter * M_PI);
+    delta.right = (readingsDiff.right / odom->tpr) * (chassisDiameter * M_PI);
+    delta.rear = (readingsDiff.rear / odom->tpr) * (chassisDiameter * M_PI);
+
+    // Delta theta
+    delta.theta = (readingsDiff.left - readingsDiff.right) / chassisDiameter;
+
+    // Local coordinates
+    double localOffsetX = delta.rear;
+    double localOffsetY = delta.left;
+    if (delta.left != delta.right) {
+        localOffsetX = 2 * sin(delta.theta / 2) * (delta.rear / delta.theta + scales.rearRadius);
+        localOffsetY = 2 * sin(delta.theta / 2) * (delta.left / delta.theta + (delta.left > delta.right ? scales.rightRadius : scales.leftRadius));
+    }
+
+    // Polar coordinates
+    double polarRadius = sqrt(localOffsetX * localOffsetX + localOffsetY * localOffsetY);
+    double polarAngle =  atan2(localOffsetY, localOffsetX) - (odom->getPose().theta + delta.theta / 2);
+
+    // Global coordinates
+    double deltaGlobalX = cos(polarAngle) * polarRadius;
+    double deltaGlobalY = sin(polarAngle) * polarRadius;
+
+    if (isnan(deltaGlobalX)) deltaGlobalX = 0;
+    if (isnan(deltaGlobalY)) deltaGlobalY = 0;
+    if (isnan(delta.theta)) delta.theta = 0;
+
+    double globalX = odom->getPose().x + deltaGlobalX;
+    double globalY = odom->getPose().y + deltaGlobalY;
+    double globalTheta = odom->getPose().theta + delta.theta;
+    odom->setPose({globalX, globalY, globalTheta, pros::millis()});
+
+    odom->prev = readings;
+}
---
+++ src/cautiontape/controller/odometry.hpp
@@ -0,0 +1,124 @@
+#include "api.h"
+#include "pros/rtos.h"
+
+namespace lamaLib {
+using namespace pros;
+
+/**
+ * @brief The coordinate position of the robot in inches
+ *
+ * Includes the time of the position
+ */
+typedef struct {
+    double x;
+    double y;
+    double theta;
+    uint time;
+} Pose;
+
+/**
+ * @brief The encoder values of all 3 tracking wheels in ticks
+ */
+typedef struct {
+    double left;
+    double right;
+    double rear;
+    double theta;
+} EncoderValues;
+
+/**
+ * @brief The measurements of the tracking wheels in inches
+ * 
+ * Left and right are separate in the case that they are different
+ */
+typedef struct {
+    double wheelDiameter;
+    double leftRadius;
+    double rightRadius;
+    double rearRadius;
+} OdomScales;
+
+class Odometry {
+    public:
+    /**
+     * @brief Odometry to keep track of the robot's position
+     * 
+     * @param leftEncoder The left encoder sensor
+     * @param rightEncoder The right encoder sensor
+     * @param rearEncoder The rear encoder sensor
+     * @param scales The measurements of the tracking wheels in inches
+     */
+    Odometry(ADIEncoder leftEncoder, ADIEncoder rightEncoder, ADIEncoder rearEncoder, OdomScales scales, int tpr);
+
+    /**
+     * @brief Get the left encoder tick counts
+     * 
+     * @return int 
+     */
+    EncoderValues getEncoders();
+
+    // Move get and set pose to private and then create a setStartPose for setting the start position
+    /**
+     * @brief Get the current coordinates
+     * 
+     * @return Pose 
+     */
+    Pose getPose();
+    /**
+     * @brief Set the pose to a new coordinate position
+     * 
+     * @param ipose The new coordinate position
+     */
+    void setPose(Pose ipose);
+
+    /**
+     * @brief Get the current tracking wheel measurements
+     * 
+     * @return OdomScales 
+     */
+    OdomScales getScales();
+    /**
+     * @brief Set the tracking wheel measurements with new measurements
+     * 
+     * @param iscales the new measurements of the tracking wheel
+     */
+    void setScales(OdomScales iscales);
+
+    /**
+     * @brief Calibrate the tracking wheels
+     *
+     * @return OdomScales
+     */
+    OdomScales calibrate();
+
+    /**
+     * @brief Starts the odometry task
+     */
+    void startOdom();
+    /**
+     * @brief Ends the odometry task
+     */
+    void endOdom();
+    
+    EncoderValues prev {0, 0, 0, 0};
+
+    int tpr;
+
+    private:
+    ADIEncoder leftEncoder;
+    ADIEncoder rightEncoder;
+    ADIEncoder rearEncoder;
+
+    Pose pose;
+    OdomScales scales;
+
+    task_t odom {};
+};
+
+extern Odometry odom;
+
+/**
+ * @brief Main function of odometry
+ */
+void odometry(void* param);
+} // namespace lamaLib
