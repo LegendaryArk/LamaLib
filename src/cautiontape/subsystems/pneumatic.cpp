c22d001e25b473121cd8181f2426aa530ac486a2
Noah <noahsun0418@gmail.com>
2022-03-15 20:41:47 -0400

    completed the class
    still needs to be initialised and tested

---
+++ src/cautiontape/subsystems/pneumatic.cpp
@@ -0,0 +1,23 @@
+#include "pneumatic.hpp"
+
+using namespace lamaLib;
+
+Pneumatic::Pneumatic(pros::ADIDigitalOut pneumatic) : pneumatic(pneumatic) {}
+
+void Pneumatic::setState(bool istate) {
+    pneumatic.set_value(istate);
+}
+void Pneumatic::open() {
+    setState(true);
+}
+void Pneumatic::close() {
+    setState(false);
+}
+void Pneumatic::toggle() {
+    setState(state);
+    state = !state;
+}
+
+bool Pneumatic::getState() {
+    return state;
+}
---
+++ src/cautiontape/subsystems/pneumatic.hpp
@@ -0,0 +1,40 @@
+#include "api.h"
+
+namespace lamaLib {
+class Pneumatic {
+    public:
+    Pneumatic(pros::ADIDigitalOut pneumatic);
+
+    /**
+     * @brief Sets the state of the pneumatic to either opened or closed
+     * 
+     * @param istate true = open; false = close
+     */
+    void setState(bool istate);
+    /**
+     * @brief Opens the pneumatic
+     */
+    void open();
+    /**
+     * @brief Closes the pneumatic
+     */
+    void close();
+    /**
+     * @brief Toggles the pneumatic between opened and closed 
+     */
+    void toggle();
+
+    /**
+     * @brief Gets the state of the pneumatic
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool getState();
+
+    private:
+    pros::ADIDigitalOut pneumatic;
+
+    bool state;
+};
+} // namespace lamaLib
