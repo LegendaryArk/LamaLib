03ca719c9f43407b9e79f1e8ae256a4e56ee862f
Noah <noahsun0418@gmail.com>
2022-03-23 19:00:22 -0400

    Added documentation

--- src/cautiontape/subsystems/motorgroup.hpp
+++ src/cautiontape/subsystems/motorgroup.hpp
@@ -2,21 +2,53 @@
 
 #include "motor.hpp"
 
+namespace lamaLib {
+/**
+ * @brief A structure to assist in returning the motor velocities
+ */
 typedef struct {
     double motorVels[];
 } MotorVels;
-
-namespace lamaLib {
 class MotorGroup {
     public:
+    /**
+     * @brief A group of motors that have similar properties, such as always being powered at the same time, same gearbox, etc.
+     * 
+     * @param motors A vector of the motors that are in the group
+     */
     MotorGroup(std::vector<Motor> motors);
 
+    /**
+     * @brief Sets all the motors to a specific velocity with a built-in PID
+     * 
+     * @param ivel The desired velocity in rpm
+     */
     void moveVelocity(int ivel);
+    /**
+     * @brief Sets all the motors to a specific voltage
+     * 
+     * @param ivolt The desired voltage in mV
+     */
     void moveVoltage(int ivolt);
 
+    /**
+     * @brief Gets the Actual Velocity of the motors in an array
+     * 
+     * @return MotorVels 
+     */
     MotorVels getActualVelocity();
 
+    /**
+     * @brief Sets the brake mode of the motors
+     * 
+     * @param brakeMode The new brake mode value from okapi that is to be set to the motors
+     */
     void setBrakeMode(okapi::AbstractMotor::brakeMode brakeMode);
+    /**
+     * @brief Gets the brake mode of the motors
+     * 
+     * @return okapi::AbstractMotor::brakeMode 
+     */
     okapi::AbstractMotor::brakeMode getBrakeMode();
 
     private:
