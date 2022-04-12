6209082647556cb263add3f6c4500fefafa41a0e
Noah <noahsun0418@gmail.com>
2022-04-12 10:02:56 -0400

    Merge branch 'inertial'

---
+++ src/cautiontape/subsystems/inertial.cpp
@@ -0,0 +1,128 @@
+#include "inertial.hpp"
+#include "pros/imu.h"
+#include "pros/rtos.h"
+
+using namespace lamaLib;
+
+Inertial::Inertial(int iport) : pros::IMU(iport) {
+    calibrating = false;
+
+    startCorrection();
+}
+
+void Inertial::resetAll() {
+    this->tare();
+}
+
+pros::c::imu_gyro_s_t Inertial::getDeltaAngles() {
+    return this->get_gyro_rate();
+}
+
+double Inertial::getRoll() {
+    return this->get_roll();
+}
+void Inertial::setRoll(double iangle) {
+    this->set_roll(iangle);
+}
+
+double Inertial::getPitch() {
+    return this->get_pitch();
+}
+void Inertial::setPitch(double iangle) {
+    this->set_pitch(iangle);
+}
+
+double Inertial::getYaw() {
+    return this->get_yaw();
+}
+void Inertial::setYaw(double iangle) {
+    this->set_yaw(iangle);
+}
+double Inertial::getHeading() {
+    return this->get_heading();
+}
+void Inertial::setHeading(double iangle) {
+    this->set_heading(iangle);
+}
+
+Angles Inertial::calibrate() {
+    if (calibrating) return {0, 0, 0};
+
+    calibrating = true;
+    pros::lcd::print(1, "Calibrating...");
+
+    this->reset();
+    pros::delay(2500);
+
+    int count = 0;
+    Angles maxDrift {0, 0, 0};
+    while (count < 100) {
+        double deltaRoll = fabs(getDeltaAngles().x);
+        double deltaPitch = fabs(getDeltaAngles().y);
+        double deltaYaw = fabs(getDeltaAngles().z);
+        
+        if (deltaRoll > maxDrift.x)
+            maxDrift.x = deltaRoll;
+        if (deltaPitch > maxDrift.y)
+            maxDrift.y = deltaPitch;
+        if (deltaYaw > maxDrift.z) 
+            maxDrift.z = deltaYaw;
+
+        pros::delay(100);
+        count++;
+    }
+    if (maxDrift.x > 0.5)
+        maxDrift.x = 0.5;
+    if (maxDrift.y > 0.5)
+        maxDrift.y = 0.5;
+    if (maxDrift.z > 0.5)
+        maxDrift.z = 0.5;
+
+    calibrating = false;
+    pros::lcd::print(1, "Calibration Complete");
+    return maxDrift;
+}
+bool Inertial::isCalibrating() {
+    return calibrating;
+}
+
+void Inertial::startCorrection() {
+    inertialTask = pros::c::task_create(driftCompensation, this, TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "inertial");
+}
+void Inertial::endCorrection() {
+    pros::c::task_delete(inertialTask);
+}
+
+void lamaLib::driftCompensation(void* iparam) {
+    Angles maxDrift = inertial.calibrate();
+
+    while (true) {
+        Angles angles = {inertial.getRoll(), inertial.getPitch(), inertial.getYaw()};
+        Angles prev = angles;
+        Angles delta = {inertial.getDeltaAngles().x, inertial.getDeltaAngles().y, inertial.getDeltaAngles().z};
+        Angles errors;
+
+        if (delta.x > maxDrift.x) {
+            if (angles.x != prev.x)
+                errors.x = prev.x - angles.x;
+        }
+        if (delta.y > maxDrift.y) {
+            if (angles.y != prev.y)
+                errors.y = prev.y - angles.y;
+        }
+        if (delta.z > maxDrift.z) {
+            if (angles.z != prev.z)
+                errors.z = prev.z - angles.z;
+        }
+
+        prev = angles;
+
+        inertial.setRoll(angles.x + errors.x);
+        inertial.setPitch(angles.y + errors.y);
+        inertial.setYaw(angles.z + errors.z);
+
+        std::cout << "x: " << inertial.getRoll() << "\ty: " << inertial.getPitch() << "\tz: " << inertial.getHeading() << "\n";
+
+        pros::delay(10);
+    }
+}
---
+++ src/cautiontape/subsystems/inertial.hpp
@@ -0,0 +1,135 @@
+#pragma once
+
+#include "api.h"
+#include "pros/imu.h"
+#include "pros/rtos.h"
+
+namespace lamaLib {
+
+/**
+ * @brief The orientation of the inertial sensor along the x, y, and z axis, in degrees
+ */
+typedef struct {
+    double x;
+    double y;
+    double z;
+} Angles;
+
+class Inertial : pros::IMU {
+    public:
+    /**
+     * @brief The inertial sensor to keep track of the robot's orientation
+     * 
+     * @param port The port that the inertial sensor is connected to
+     */
+    Inertial(int port);
+
+    /**
+     * @brief Reset all the readings back to 0
+     */
+    void resetAll();
+
+    /**
+     * @brief Gets the delta of the readings
+     * 
+     * @return pros::c::imu_gyro_s_t 
+     */
+    pros::c::imu_gyro_s_t getDeltaAngles();
+
+    /**
+     * @brief Gets the roll, or the orientation about the x axis in degrees
+     * 
+     * @return double 
+     */
+    double getRoll();
+    /**
+     * @brief Sets the roll, or the orientation about the x axis in degrees
+     * 
+     * @param iangle The new angle in degrees
+     */
+    void setRoll(double iangle);
+
+    /**
+     * @brief Gets the pitch, or the orientation about the y axis in degrees
+     * 
+     * @return double 
+     */
+    double getPitch();
+    /**
+     * @brief Sets the pitch, or the orientation about the y axis in degrees
+     * 
+     * @param iangle The new angle in degrees
+     */
+    void setPitch(double iangle);
+
+    /**
+     * @brief Gets the yaw, or the orientation about the z axis in degrees
+     *
+     * Better known as the heading
+     * 
+     * @return double 
+     */
+    double getYaw();
+    /**
+     * @brief Sets the yaw, or the orientation about the z axis in degrees
+     *
+     * Better known as the heading
+     * 
+     * @param iangle The new angle in degrees
+     */
+    void setYaw(double iangle);
+    /**
+     * @brief Gets the yaw, or the orientation about the z axis in degrees, clamped between 0 and 360
+     *
+     * Better known as the heading
+     * 
+     * @return double 
+     */
+    double getHeading();
+    /**
+     * @brief Sets the yaw, or the orientation about the z axis in degrees, clamped between 0 and 360
+     *
+     * Better known as the heading
+     * 
+     * @param iangle The new angle in degrees
+     */
+    void setHeading(double iangle);
+
+    /**
+     * @brief Calibrates the inertial sensor, finding the max drifts about each axis
+     * 
+     * @return Angles 
+     */
+    Angles calibrate();
+    /**
+     * @brief Returns whether the sensor is calibrating or not
+     * 
+     * @return true 
+     * @return false 
+     */
+    bool isCalibrating();
+
+    /**
+     * @brief Starts the drift correction task
+     */
+    void startCorrection();
+    /**
+     * @brief Ends the drift correction task
+     */
+    void endCorrection();
+
+    private:
+    pros::task_t inertialTask {};
+
+    bool calibrating;
+};
+
+extern Inertial inertial;
+
+/**
+ * @brief The function that corrects the readings from the inertial sensor in the case of gyro drift
+ * 
+ * @param iparam 
+ */
+void driftCompensation(void* iparam);
+} // namespace lamaLib
