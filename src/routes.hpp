712a0f0cfca3796558647491311bc278b07d787d
Noah <noahsun0418@gmail.com>
2022-04-23 17:05:55 -0400

    routes

---
+++ src/routes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "cautiontape/lamaapi.hpp"
+
+void startLeftMidGoal();
+void startLeftLeftGoal();
+void startRightRightGoal();
+void startLeftFullAWP();
+void startRightFullAWP();
