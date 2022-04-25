658b24c208a68d6ca161d575d51709c53bca291d
Noah <noahsun0418@gmail.com>
2022-04-24 20:06:42 -0400

    added routes methods

--- src/routes.cpp
+++ src/routes.cpp
@@ -1,1 +1,19 @@
 #include "routes.hpp"
+
+void startLeftMidGoal() {
+
+}
+void startLeftLeftGoal() {
+
+}
+
+void startRightRightGoal() {
+
+}
+
+void startLeftFullAWP() {
+
+}
+void startRightFullAWP() {
+
+}
